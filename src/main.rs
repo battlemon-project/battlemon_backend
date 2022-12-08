4452398375b498890ac84834ed32a56c56699318
ChzenChzen <fedorovdanila@me.com>
2022-12-09 00:15:36 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use battlemon_backend::startup::App;
+use battlemon_backend::{config, telemetry};
+use color_eyre::eyre::Context;
+use color_eyre::Result;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let subscriber =
+        telemetry::get_subscriber("battlemon_backend".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber).context("Failed to init tracing subscriber")?;
+    info!("Loading application config");
+    let config = config::load_config().context("Failed to load app config")?;
+    let app = App::build(config).await?;
+    app.run_until_stopped().await?;
+
+    Ok(())
+}
