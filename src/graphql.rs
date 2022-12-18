632c35ee9234dcaca7c9e5f15eb96749e3cfbf45
ChzenChzen <fedorovdanila@me.com>
2022-12-18 18:29:45 +0700

    - add helper to query db

--- src/graphql.rs
+++ src/graphql.rs
@@ -18,6 +18,15 @@ impl QueryRoot {
 
         Ok(tokens)
     }
+
+    async fn nft_token(&self, ctx: &Context<'_>, id: String) -> Result<NftToken> {
+        let pool = ctx.data_unchecked::<PgPool>();
+        let token = get_nft_token_db(id, pool)
+            .await
+            .context("Failed to get nft token data from database")?;
+
+        Ok(token)
+    }
 }
 
 #[tracing::instrument(name = "Query nft tokens from database", skip_all)]
@@ -29,5 +38,18 @@ async fn get_nft_tokens_db(pool: &PgPool) -> StdResult<Vec<NftToken>, sqlx::Erro
         "#
     )
     .fetch_all(pool)
+    .await
+}
+
+#[tracing::instrument(name = "Query nft tokens from database", skip(pool))]
+async fn get_nft_token_db(id: String, pool: &PgPool) -> StdResult<NftToken, sqlx::Error> {
+    query_as!(
+        NftToken,
+        r#"
+        SELECT id, type, owner, url FROM nft_tokens WHERE id = $1
+        "#,
+        id
+    )
+    .fetch_one(pool)
     .await
 }
